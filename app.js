fac7f90b46f1ba761054a7970641fcf9489b8121
Ben Huber <benkhuber@gmail.com>
2024-08-30 16:05:05 -0700

    init

---
+++ app.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+const express = require('express')
+const axios = require('axios');
+
+const app = express()
+const port = process.env.PORT
+const pool = require('./db');
+const processJob = require('./utils/jobProcessor')
+
+
+app.get('/health', (req, res) => {
+  res.send('OK')
+})
+
+app.get('/jobs', async (req, res) => {
+    try {
+        const response = await pool.query('SELECT * FROM jobs');
+        res.json(response.rows);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).send('Server error');
+    }
+})
+
+app.get('/fetch_jobs', async (req, res) => {
+    const url = process.env.API_URL;
+
+    try {
+        await pool.query('DELETE FROM jobs');
+        console.log('Deleted all existing job postings from DB');
+
+        const response = await axios.get(url);
+
+        if (response.status === 200) {
+            const jobs = response.data.data;
+
+            for (const job of jobs) {
+                processJob(job)
+            }
+            res.send(`Status: ${response.status}, collecting jobs...`);
+        } else {
+            console.log(`Request failed: ${response.status}`);
+            res.status(response.status).send(`Request failed: ${response.status}`);
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).send('Server error');
+    }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
